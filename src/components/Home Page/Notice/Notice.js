fd26b7623dcc61305a84442ea4408050463793fa
yasith <it19966922@my.sliit.lk>
2022-04-05 22:34:48 +0530

    create notice component

---
+++ src/components/Home Page/Notice/Notice.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Notice.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Notice = () => {
+  return (
+    <div className="notice-container">
+      <div>
+        <p className="text-center">Notices</p>
+      </div>
+    </div>
+  );
+};
+
+export default Notice;
