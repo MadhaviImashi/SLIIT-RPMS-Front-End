e2177e55d19656433fb5a4248b94d9d796eeb0d1
MadhaviImashi <madhaviimashi@gmail.com>
2022-04-13 01:27:55 +0530

    add home page to App.js

---
+++ src/components/Home Page/Home.js
@@ -0,0 +1,13 @@
+import Notice from './Notice/Notice';
+import About from './About/About';
+
+const Home = () => {
+  return (
+    <div>
+      <About />
+      <Notice />
+    </div>
+  );
+};
+
+export default Home;
