4c227a809e530a043c312c59cb53a251995f0160
yasith <it19966922@my.sliit.lk>
2022-04-13 09:09:48 +0530

    update components

--- src/components/Home Page/Notice/NoticeCard.js
+++ src/components/Home Page/Notice/NoticeCard.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import './Notice.css';
 const NoticeCard = () => {
   return (
-    <div className="notice-card">
-      notice card
-      <div>NAme</div>
+    <div className="box red col-lg-2 col-md-12 col-sm-2">
+      <h2>Team Builder</h2>
+      <p>Scans our talent network to create the optimal team for your project</p>
     </div>
   );
 };
