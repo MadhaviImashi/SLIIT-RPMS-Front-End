6b63b0a511cf8ad248477b02bf2ed4a60a1d391a
yasith <it19966922@my.sliit.lk>
2022-04-14 10:31:51 +0530

    create footer component

--- src/components/Home Page/Home.js
+++ src/components/Home Page/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import About from './About/About';
 import HomeCarousel from './Carousel/Carousel';
+import Notice from './Notice/Notice';
+import Footer from '../../shared/Footer/Footer';
 
 const Home = () => {
   return (
     <div>
       <HomeCarousel />
       <About />
+      <Notice />
+      <Footer />
     </div>
   );
 };
