e44a7379d70a54c58ada96c15b6061faa24af986
MadhaviImashi <madhaviimashi@gmail.com>
2022-04-25 11:22:57 +0530

    remove unwanted code

--- src/components/LoginPage/Login.js
+++ src/components/LoginPage/Login.js
@@ -1,6 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Login.css';
-import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
@@ -9,7 +8,7 @@ import FormControl from '@mui/material/FormControl';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import { useState } from 'react';
-import { FormControlLabel, TextField } from '@mui/material';
+import { FormControlLabel } from '@mui/material';
 import { Checkbox } from 'antd';
 import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 
@@ -38,62 +37,6 @@ const Login = () => {
   };
 
   return (
-    // <div className="row container">
-    //   <div className="col-md-6 col-lg-6">
-    //     <div className="login-form">
-    //       <div className="heading-main">Login to SLIIT RPMS</div>
-    //       <div className="sub-heading">
-    //         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cupiditate doloremque
-    //         dolores enim eum excepturi, exercitationem facere fugit ipsa laborum libero magni nihil,
-    //         provident quam qui repellendus sint totam, veniam.
-    //       </div>
-    //       <form className="login-form mt-5">
-    //         <Box
-    //           component="form"
-    //           sx={{
-    //             '& .MuiTextField-root': { m: 1, width: '80%' }
-    //           }}
-    //           noValidate
-    //           autoComplete="off">
-    //           <TextField id="outlined-email-input" label="Email" type="email" />
-    //           <FormControl sx={{ m: 1, width: '80%' }} variant="outlined">
-    //             <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-    //             <OutlinedInput
-    //               id="outlined-adornment-password"
-    //               type={values.showPassword ? 'text' : 'password'}
-    //               value={values.password}
-    //               onChange={handleChange('password')}
-    //               endAdornment={
-    //                 <InputAdornment position="end">
-    //                   <IconButton
-    //                     aria-label="toggle password visibility"
-    //                     onClick={handleClickShowPassword}
-    //                     onMouseDown={handleMouseDownPassword}
-    //                     edge="end">
-    //                     {values.showPassword ? <VisibilityOff /> : <Visibility />}
-    //                   </IconButton>
-    //                 </InputAdornment>
-    //               }
-    //               label="Password"
-    //             />
-    //           </FormControl>
-    //           <div className="d-flex justify-content-between forgot-password">
-    //             <FormControlLabel
-    //               sx={{ m: 1, width: 'auto' }}
-    //               control={
-    //                 <Checkbox defaultChecked sx={{ '& .MuiSvgIcon-root': { fontSize: 28 } }} />
-    //               }
-    //               label="Remember Me"
-    //             />
-    //             <a href="#"> forgot password</a>
-    //           </div>
-    //           <input type="submit" value="Login" className="btn btn-primary btn-block submit-btn" />
-    //         </Box>
-    //       </form>
-    //     </div>
-    //   </div>
-    //   <div className="col-md-6 col-lg-6 right-img"></div>
-    // </div>
     <div>
       <Container className="login-outer-wrapper">
         <Row className="login-container no-gutters">
@@ -102,7 +45,7 @@ const Login = () => {
           </Col>
           <Col md className="login-form-wrapper">
             <Form>
-              <h1 className="form-title mb-3 mt-4">Login</h1>
+              <h1 className="form-title mb-3 mt-4">Sign In</h1>
 
               <p className="sub-title mb-4">Sign in to your SLIIT RPMS account</p>
 
@@ -132,7 +75,7 @@ const Login = () => {
                 />
               </FormControl>
 
-              <div className="forgot-password mb-3">
+              <div className="forgot-password mb-4">
                 <FormControlLabel control={<Checkbox defaultChecked />} label="Remember Me" />
                 <a href="#"> forgot password</a>
               </div>
