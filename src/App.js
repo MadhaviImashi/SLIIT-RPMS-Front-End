6ffd0177349e659c5237b2d58398c9a27b590dc8
yasith <it19966922@my.sliit.lk>
2022-04-26 18:45:56 +0530

    create routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home Page/Home';
+import SignUp from './components/SignUpPage/Student/SignUp';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/student-register" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
