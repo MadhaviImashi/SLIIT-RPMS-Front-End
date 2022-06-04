ac9e0b409c04fdfcb37c5e945ef40a91f348b88f
MadhaviImashi <madhaviimashi@gmail.com>
2022-06-04 10:08:00 +0530

    About section of Home page

---
+++ src/components/Home Page/About/AboutItem.js
@@ -0,0 +1,10 @@
+const AboutItem = (prop) => {
+    return (
+        <div className="about-item-container">
+            <div className="about-item-icon">{prop.icon}</div>
+            <h4 className="about-item-title">{prop.itemTitle}</h4>
+            <p className="about-item-description">{prop.itemDescription}</p>
+        </div>
+    );
+};
+export default AboutItem;
