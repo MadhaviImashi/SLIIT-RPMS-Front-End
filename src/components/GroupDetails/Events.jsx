b34eaa6df652a718c6c7272ff5d8021fb1cee508
yasith <it19966922@my.sliit.lk>
2022-06-04 10:08:12 +0530

    implement group details page

---
+++ src/components/GroupDetails/Events.jsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Events = () =>{
+    return (<div>
+            <div className="me-2 p-1 mt-2" style={{borderLeft:"5px solid green", borderTop:"1px solid green", borderRight:"1px solid green", borderBottom:"1px solid green",fontSize:"0.8rem"}}>
+                <span className=" rounded-circle bg-success text-white fw-200" style={{padding:"3px 5px 3px 5px"}}> 21 </span><br></br>
+                Report 
+                submission
+            </div>
+    </div>)
+}
+
+export default Events;
